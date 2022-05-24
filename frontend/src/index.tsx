26017ea5e287961c5ec889bc3ed73d2ff1815b84
felipedutra13 <dutra.felipe92@gmail.com>
2022-05-24 08:30:26 -0300

    static charts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -9,4 +11,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); 
